bfa73d87fa3990035757f7347ff6722d59aff269
ths20734 <seroro616kazu@gmail.com>
2016-02-10 20:07:32 +0900

    scala 01

--- Hellow/app/controllers/Application.scala
+++ Hellow/app/controllers/Application.scala
@@ -2,11 +2,20 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data.Form
+import play.api.data.Forms._
 
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+   Ok(views.html.test("test"))
+  }
+val form = Form( "name" -> text )
+val form2 = Form( "pass" -> text )
+  def formSample = Action { implicit request =>
+    val name = form.bindFromRequest.get
+    val pass = form2.bindFromRequest.get
+    Ok(name+pass)
   }
 
 }
